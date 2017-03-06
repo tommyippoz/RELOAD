a72f88091617d3917ea139b5ee6d738c99e97764
Tommaso Zoppi <tommaso.zoppi@unifi.it>
2017-03-06 17:18:27 +0100

    First Commons Commit

---
+++ MADneSs_Commons/src/ippoz/multilayer/commons/datacategory/DataCategory.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ippoz.multilayer.commons.datacategory;
+
+/**
+ * The Enum DataCategory.
+ *
+ * @author Tommy
+ */
+public enum DataCategory {
+	
+	/** The plain data category: the normal observation. */
+	PLAIN,
+	
+	/** The difference data category: difference between current and previous observation. */
+	DIFFERENCE,
+	 
+	/** The default - null - data category. */
+	NULL
+}
