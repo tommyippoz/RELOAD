f9beb7c755c42ceee7f7db0e8d4e70fd7a608243
Tommaso Zoppi <tommaso.zoppi@unifi.it>
2019-04-15 16:17:05 +0200

    UML Diagrams

---
+++ RELOAD/src/ippoz/reload/loader/ARFFLoader.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package ippoz.reload.loader;
+
+import ippoz.reload.commons.knowledge.data.MonitoredData;
+
+import java.util.List;
+
+/**
+ * @author Tommy
+ *
+ */
+public class ARFFLoader extends SimpleLoader {
+
+	public ARFFLoader(List<Integer> runs) {
+		super(runs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<MonitoredData> fetch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRuns() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
